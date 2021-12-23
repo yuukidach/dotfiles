ab5c3f3b9a0e72133b7d3b096ccf4026b3154b5f
yuukidach <chendamailbox@foxmail.com>
2021-12-23 23:32:01 +0800

    style(polybar): apply forest theme to polybar

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+DIR="$HOME/.config/polybar/forest"
+
 # Terminate already running bar instances
 killall -q polybar
 # If all your bars have ipc enabled, you can also use 
@@ -7,7 +9,8 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar yuukidach 2>&1 | tee -a /tmp/polybar1.log & disown
+#polybar yuukidach 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar -q main -c "$DIR"/config 2>&1 | tee -a /tmp/polybar1.log &disown
 
 echo "Bars launched..."
 
